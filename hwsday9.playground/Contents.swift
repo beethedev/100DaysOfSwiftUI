33cc964b7789c424828dd82b31355e02f684906d
Busayo O <44685320+beethedev@users.noreply.github.com>
2023-10-24 15:11:15 -0700

    hwsday9 commit
    Checkpoint 5

---
+++ hwsday9.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+//Closures and Checkpoint5
+//Your job is to:
+//Filter out any numbers that are even
+//Sort the array in ascending order
+//Map them to strings in the format “7 is a lucky number”
+//Print the resulting array, one item per line
+//So, your output should be as follows:
+//7 is a lucky number
+//15 is a lucky number
+//21 is a lucky number
+//31 is a lucky number
+//33 is a lucky number
+//49 is a lucky number
+
+//Solution
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+func filterSortMap(no:[Int]) -> [String] {
+    return no.filter{$0.isMultiple(of: 2) == false}.sorted().map{String("\($0) is a lucky number")}
+    
+}
+
+for luckyNumber in filterSortMap(no: luckyNumbers){
+    print(luckyNumber)
+}
+//print(result)

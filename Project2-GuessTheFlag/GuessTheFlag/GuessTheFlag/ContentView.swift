59ee447569f865c855b7f2ab6aa6d6f239a67fb0
Busayo O <44685320+beethedev@users.noreply.github.com>
2023-11-04 16:59:18 -0700

    Challenge solutions added
    Updated contentview with solutions to GuessTheFlag solutions

--- Project2-GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project2-GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
+    @State private var restartGame = false
     @State private var scoreTitle = ""
+    @State private var userScore = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-
+    
+    @State private var count = 0
+    @State private var messageText = ""
     
     var body: some View {
         
@@ -35,7 +39,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(userScore)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
                 
@@ -68,7 +72,12 @@ struct ContentView: View {
                 .alert(scoreTitle, isPresented: $showingScore) {
                     Button("Continue", action: askQuestion)
                 } message: {
-                    Text("Your score is ???")
+                    Text(messageText)
+                }
+                .alert("Game Over", isPresented: $restartGame){
+                    Button("Restart", action: restart)
+                } message: {
+                    Text("Your Final Score is \(userScore)")
                 }
                 
             }
@@ -79,18 +88,33 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1 //Challenge 1 solution
+            messageText = "Your Score is \(userScore)"
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong, that is the flag for \(countries[number])" //Challenge 2 solution
         }
 
         showingScore = true
     }
     
     func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        count += 1
+        if count < 8 {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        }
+        if count == 8{
+            restartGame = true
+        }
     }
-}
+    
+    
+    func restart() {
+        userScore = 0
+        count = 0
+        askQuestion()
+        }
+    }
 
 #Preview {
     ContentView()

cd75c94cdffe6366b0ca2977dd0ea5d0961a39f3
Busayo O <44685320+beethedev@users.noreply.github.com>
2023-11-11 14:27:26 -0800

    Project 5 added
    committing WordScramble and challenge solutions

---
+++ Project5-WordScramble/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,177 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Busayo Ajide on 11/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var score = 0
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                                Image(systemName: "\(word.count).circle")
+                                Text(word)
+                            }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK") { }
+            } message: {
+                Text(errorMessage)
+            }
+            .toolbar{
+                Button("New word", action: startGame)
+            }
+            
+            Spacer()
+            
+            VStack{
+                Text("Score - \(score)")
+                    .font(.largeTitle)
+            }
+        }
+    }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else { return }
+
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        guard tooShort(word: answer) else {
+            wordError(title: "Word too short", message: "You need to use at least three letters")
+            return
+        }
+        
+        guard sameWord(word: answer) else {
+            wordError(title: "Same word", message: "You can't use the start word")
+            return
+        }
+        
+        // extra validation to come
+
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        //score += 1
+        addPoints(answer.count)
+        newWord = ""
+    }
+    
+    func startGame() {
+        //Reset score
+        score = 0
+        usedWords.removeAll()
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+
+                // If we are here everything has worked, so we can exit
+                return
+            }
+        }
+
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func tooShort(word: String) -> Bool {
+        word.count > 2
+    }
+    
+    func sameWord(word: String) -> Bool {
+        word != rootWord
+    }
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    func addPoints(_ number:Int){
+        if number == 3 {
+            score += 1
+        } else if number <= 5 {
+            score += 2
+        } else {
+            score += 3
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project5-WordScramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Busayo Ajide on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

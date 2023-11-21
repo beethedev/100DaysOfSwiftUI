8d2858aeb754fd924db70e0b57ffd8395411a773
Busayo O <44685320+beethedev@users.noreply.github.com>
2023-11-20 20:17:10 -0800

    Project7-iExpense initial commit

---
+++ Project7-iExpense/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Busayo Ajide on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

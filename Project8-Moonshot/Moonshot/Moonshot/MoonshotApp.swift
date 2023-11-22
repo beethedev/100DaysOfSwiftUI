c4adf5873eb086bb8b1822ee12763fbfaded05ab
Busayo O <44685320+beethedev@users.noreply.github.com>
2023-11-21 20:41:55 -0800

    Project 8 initial commit

---
+++ Project8-Moonshot/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Busayo Ajide on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

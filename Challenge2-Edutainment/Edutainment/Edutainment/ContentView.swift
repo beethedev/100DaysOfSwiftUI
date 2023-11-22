f95407703e4ac6c9b4065ec1c3fe7f7279e18a7b
Busayo O <44685320+beethedev@users.noreply.github.com>
2023-11-21 20:41:28 -0800

    updated app

--- Challenge2-Edutainment/Edutainment/Edutainment/ContentView.swift
+++ Challenge2-Edutainment/Edutainment/Edutainment/ContentView.swift
@@ -8,45 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var noToMultiply = 5
-    @State private var question = 10
+    @State private var noToMultiply = 3
+    @State private var question = 5
     @State private var answer = 0
     @State private var score = 0
     
     @State private var correctAnswer = 0
-    @State private var number = Int.random(in: 2...25)
+    @State private var number = Int.random(in:0..<15)
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingScore = false
     
     @State private var count = 0
     @State private var restartGame = false
+    @State private var icons = ["bear", "buffalo", "chick", "chicken", "cow", "crocodile", "dog", "duck", "elephant", "frog", "giraffe", "goat","gorilla", "hippo", "horse", "monkey", "moose"].shuffled()
+    
+    @State private var animateIcon = false
+    @State private var animationAmount = 0.0
     
     let questions = [5, 10, 20]
     //static let multipliers = 2...30
     
     var body: some View {
         NavigationStack{
-            VStack{
+            ZStack{
                 
-                Text("Score - \(score)").font(.title2)
+                Color(.blue)
+                    .opacity(0.8)
+                    .ignoresSafeArea()
                 
                 VStack{
                     Form {
                         // Pick nos to multiply from 2 to 12
                         Section("Choose a number between 2 and 12"){
                             Stepper("\(noToMultiply)", value: $noToMultiply, in: 2...12, step:1)
-                            //HStack{
-                              //  ForEach(2..<13, id: \.self){ no in
-                                //    Button("\(no)"){
-                                  //      noToMultiply = no
-                                    //}
-                                  //  .padding()
-                                 //   .background(.blue)
-                                 //   .foregroundStyle(.white)
-                                 //   .clipShape(.capsule)
-                               // }
-                           // }
                         }
                         
                         //Pick how many numbers to practice
@@ -58,19 +53,27 @@ struct ContentView: View {
                             }
                             .pickerStyle(.segmented)
                         }
+                    }
+                
+                    VStack(spacing: 15) {
+                        //Spacer()
+                        Text("Score - \(score)").font(.title)
                         
-                        //Randomly generate questions based on 2 above
-                        Section("\(noToMultiply) times table"){
-                            VStack {
-                                //use imaages or emojis for no to multiply
-                                //Text("\(noToMultiply) x \(number + 1) = ")
-                                Text("What is \(noToMultiply) x \(number) ?")
-                                TextField("Answer", value: $answer, format: .number)
-                                    .textFieldStyle(.roundedBorder)
-                                // more to come
-                                //format answer to change to red text if value is wrong
-                            }
+                        Image(icons[number])
+                            //.animation(.default, value: animationAmount)
+                            .rotation3DEffect(
+                                .degrees(animateIcon ? animationAmount : 0.0),axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                            .animation(.default, value: animateIcon)
+                        
+                        Text("What is \(noToMultiply) x \(number) ?")
+                            .font(.title2).bold()
+                        
+                        TextField("Answer", value: $answer, format: .number)
+                            .foregroundStyle(answer == correctAnswer ? .black : .red)
+                            .textFieldStyle(.roundedBorder)
                             .onSubmit{checkAnswer()}
+                            .frame(maxWidth: 200)
                             .alert(alertTitle, isPresented: $showingScore){
                                 Button("Continue", action: askQuestion)
                             } message: {
@@ -81,30 +84,27 @@ struct ContentView: View {
                             } message: {
                                 Text("Your Final Score is \(score)")
                             }
+                        
+                        Spacer()
+                        
+                        Text("\(count) of \(question) questions answered")
+                            .fontWeight(.medium)
+                        
+                        
+                        Button("Start Over"){
+                            restart()
                         }
+                        .padding()
+                        .background(.green)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .clipShape(.capsule)
                     }
-                }
-                    
-                Text("\(count) of \(question) questions answered")
-                    .padding()
-                
-                Spacer()
-                
-                Button("New Game"){
-                        restart()
-                    }
-                    .padding()
-                    .background(.green)
                     .foregroundStyle(.white)
-                    .clipShape(.capsule)
-                    
-                Spacer()
-                Spacer()
+                }
+                .navigationTitle("Learn Times Tables")
             }
-            .navigationTitle("Learn to Multiply")
-            
         }
-        
     }
     
     func checkAnswer(){
@@ -114,27 +114,34 @@ struct ContentView: View {
             alertTitle = "Good Job!"
             alertMessage = "That is correct."
             
+            animationAmount = 360.0
+            animateIcon.toggle()
         } else {
             alertTitle = "Uh Oh!"
             alertMessage = "The correct answer is \(correctAnswer)."
         }
         showingScore = true
+        count += 1
+        //correctAnswer = 0
     }
     
     func askQuestion(){
-        count += 1
+        correctAnswer = 0
+        answer = 0
         if count < question {
-            number = Int.random(in: 2...25)
-            answer = 0
+            number = Int.random(in: 0..<15)
         }
         else {
             restartGame = true
         }
+        
     }
     
     func restart() {
         score = 0
         count = 0
+        answer = 0
+        correctAnswer = 0
         askQuestion()
         }
     

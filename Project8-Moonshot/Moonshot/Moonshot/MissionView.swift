fee43f4220f4355572c660271fae9a8522697838
Busayo O <44685320+beethedev@users.noreply.github.com>
2023-11-24 15:19:05 -0800

    updated app

---
+++ Project8-Moonshot/Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,110 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Busayo Ajide on 11/24/23.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission:Mission
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
+    
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+        
+    }
+    
+    let crew: [CrewMember]
+    
+    var body: some View {
+                ScrollView {
+                    VStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .containerRelativeFrame(.horizontal) { width, axis in
+                                width * 0.6
+                            }
+                            .padding(.top)
+
+                        VStack(alignment: .leading) {
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundStyle(.lightBackground)
+                                .padding(.vertical)
+                            Text("Mission Highlights")
+                                .font(.title.bold())
+                                .padding(.bottom, 5)
+
+                            Text(mission.description)
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundStyle(.lightBackground)
+                                .padding(.vertical)
+                            Text("Crew")
+                                .font(.title.bold())
+                                .padding(.bottom, 5)
+                        }
+                        .padding(.horizontal)
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                ForEach(crew, id: \.role) { crewMember in
+                                    NavigationLink {
+                                        AstronautView(astronaut: crewMember.astronaut)
+                                    } label: {
+                                        HStack {
+                                            Image(crewMember.astronaut.id)
+                                                .resizable()
+                                                .frame(width: 104, height: 72)
+                                                .clipShape(.capsule)
+                                                .overlay(
+                                                    Capsule()
+                                                        .strokeBorder(.white, lineWidth: 1)
+                                                )
+
+                                            VStack(alignment: .leading) {
+                                                Text(crewMember.astronaut.name)
+                                                    .foregroundStyle(.white)
+                                                    .font(.headline)
+                                                Text(crewMember.role)
+                                                    .foregroundStyle(.white.opacity(0.5))
+                                            }
+                                        }
+                                        .padding(.horizontal)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .padding(.bottom)
+                }
+                .navigationTitle(mission.displayName)
+                .navigationBarTitleDisplayMode(.inline)
+                .background(.darkBackground)
+            
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+        return MissionView(mission: missions[0], astronauts: astronauts)
+            .preferredColorScheme(.dark)
+}

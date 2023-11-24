bf138aa97d4e8ff1f9cd18a4034b96cecd40e112
Busayo O <44685320+beethedev@users.noreply.github.com>
2023-11-23 20:33:52 -0800

    Project8-moonshot update

---
+++ Project8-Moonshot/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Busayo Ajide on 11/23/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}

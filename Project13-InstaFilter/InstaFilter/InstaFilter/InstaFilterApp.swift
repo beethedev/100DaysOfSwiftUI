e8b39ffc7e39e6cc5b05c603eee5782458e449cc
Busayo O <44685320+beethedev@users.noreply.github.com>
2023-12-21 21:57:16 -0800

    Project13-Instafilter initial commit

---
+++ Project13-InstaFilter/InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Busayo Ajide on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

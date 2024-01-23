5fa4befa55dea67448ce6fcd86747cccdc0b3fdb
Busayo O <44685320+beethedev@users.noreply.github.com>
2024-01-22 21:19:27 -0800

    accessibility update

--- Project5-WordScramble/WordScramble/WordScramble/ContentView.swift
+++ Project5-WordScramble/WordScramble/WordScramble/ContentView.swift
@@ -32,7 +32,10 @@ struct ContentView: View {
                                 Image(systemName: "\(word.count).circle")
                                 Text(word)
                             }
-                    }
+                            .accessibilityElement()
+                            .accessibilityLabel(word)
+                            .accessibilityHint("\(word.count) letters")
+                        }
                 }
             }
             .navigationTitle(rootWord)
